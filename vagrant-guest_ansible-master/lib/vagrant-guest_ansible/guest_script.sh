b5efb3670fbaf8ed37f692c78d7066fce6d478fd
jssept04 <js.sept04@gmail.com>
2017-02-19 19:04:10 +0530

    having ansible only

---
+++ vagrant-guest_ansible-master/lib/vagrant-guest_ansible/guest_script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ANSIBLE_PLAYBOOK=$1
+ANSIBLE_HOSTS=$2
+ANSIBLE_EXTRA_VARS="$3"
+TEMP_HOSTS="/tmp/ansible_hosts"
+
+if [ ! -f /vagrant/$ANSIBLE_PLAYBOOK ]; then
+        echo "ERROR: Cannot find the given Ansible playbook."
+        exit 1
+fi
+
+if [ ! -f /vagrant/$ANSIBLE_HOSTS ]; then
+        echo "ERROR: Cannot find the given Ansible hosts file."
+        exit 2
+fi
+
+if ! command -v ansible >/dev/null; then
+        echo "Installing Ansible dependencies and Git."
+        if command -v yum >/dev/null; then
+                sudo yum install -y gcc git python python-devel
+        elif command -v apt-get >/dev/null; then
+                sudo apt-get update -qq
+                #sudo apt-get install -y -qq git python-yaml python-paramiko python-jinja2
+                sudo apt-get install -y -qq git python python-dev
+        else
+                echo "neither yum nor apt-get found!"
+                exit 1
+        fi
+        echo "Installing pip via easy_install."
+        wget http://peak.telecommunity.com/dist/ez_setup.py
+        sudo python ez_setup.py && rm -f ez_setup.py
+        sudo easy_install pip
+        # Make sure setuptools are installed crrectly.
+        sudo pip install setuptools --no-use-wheel --upgrade
+        echo "Installing required python modules."
+        sudo pip install paramiko pyyaml jinja2 markupsafe
+        sudo pip install ansible
+fi
+
+if [ ! -z "$ANSIBLE_EXTRA_VARS" -a "$ANSIBLE_EXTRA_VARS" != " " ]; then
+        ANSIBLE_EXTRA_VARS=" --extra-vars $ANSIBLE_EXTRA_VARS"
+fi
+
+# stream output
+export PYTHONUNBUFFERED=1
+# show ANSI-colored output
+export ANSIBLE_FORCE_COLOR=true
+
+cp /vagrant/${ANSIBLE_HOSTS} ${TEMP_HOSTS} && chmod -x ${TEMP_HOSTS}
+echo "Running Ansible as $USER:"
+ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --inventory-file=${TEMP_HOSTS} --connection=local $ANSIBLE_EXTRA_VARS
+rm ${TEMP_HOSTS}
